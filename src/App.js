bd7f8c7ff69747e491aa39aa3a687b80ff6c77be
MAwaisme <73178485+MAwaisme@users.noreply.github.com>
2023-12-07 17:01:48 +0500

    build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Suspense, useEffect, useRef, useState } from "react";
 
 const Model = () => {
 
-  // const modelRef = useRef();
+  const modelRef = useRef();
 
   let val = window.location.href;
   val = new URL(val);
@@ -15,48 +15,48 @@ const Model = () => {
 
 
   const localModl = useLoader(GLTFLoader, "https://models.readyplayer.me/63bd22c325903a9ffc56f09f.glb");
-  // const localModl1 = useLoader(GLTFLoader, modelUrl);
+  const localModl1 = useLoader(GLTFLoader, modelUrl);
 
 
-  // const [mdlURL, setMdlURL] = useState(null);
+  const [mdlURL, setMdlURL] = useState(null);
 
-  // const urlParams = new URLSearchParams(window.location.search);
-  // const id = urlParams.get('id');
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get('id');
   console.log("🚀 ~ file: App.js ~ line 16 ~ LegionModal ~ modelUrl", localModl)
 
 
   // Load the 3D model
-  // const loader = new GLTFLoader();
+  const loader = new GLTFLoader();
 
-  // loader.load(modelUrl, (gltf) => {
-  //   console.log('checking...', modelUrl, modelUrl);
-  //   const model = gltf.scene;
-  //   // Add the loaded model to the ref
-  //   modelRef.current = model;
-  //   console.log('checking...111111', modelUrl, modelUrl);
-  // });
+  loader.load(modelUrl, (gltf) => {
+    console.log('checking...', modelUrl, modelUrl);
+    const model = gltf.scene;
+    // Add the loaded model to the ref
+    modelRef.current = model;
+    console.log('checking...111111', modelUrl, modelUrl);
+  });
 
   useEffect(() => {
-    // if (!modelRef.current) {
-    //   setMdlURL(localModl);
-    // }
-    // else if (modelUrl) {
-    //   setMdlURL(modelUrl);
-    // }
-    // else {
-    //   setMdlURL(localModl);
-    // }
+    if (!modelRef.current) {
+      setMdlURL(localModl);
+    }
+    else if (modelUrl) {
+      setMdlURL(modelUrl);
+    }
+    else {
+      setMdlURL(localModl);
+    }
   }, [modelUrl])
 
 
   return (
     <>
       {!modelUrl ?
-        <primitive object={localModl.scene} scale={1} />
+        <primitive object={localModl1 ? localModl1?.scene : localModl.scene} scale={1} />
         :
         <>
           {modelUrl ?
-            <primitive object={localModl.scene} scale={1} />
+            <primitive object={localModl1 ? localModl1?.scene : localModl.scene} scale={1} />
             :
             <Html style={{ width: '100%', height: '400px' }}>
               <b className='loading-text' style={{ width: '100px', display: 'flex' }}>Please wait it's takeing a time...</b>
@@ -78,7 +78,7 @@ export default function App() {
             <Model scale={[0.1, 0.1, 0.1]} />
           </Stage>
           <OrbitControls />
-          {/* <Environment preset="city" background /> */}
+          <Environment preset="city" background />
         </Suspense>
         <OrbitControls enableZoom={true} enablePan={true} autoRotate />
       </Canvas>
